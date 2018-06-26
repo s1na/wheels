838a0fee0b54eaacf64dd9159a18528d800ffade
Sina Mahmoodi <itz.s1na@gmail.com>
2018-06-26 22:08:30 +0200

    Serve react with babel and hot loader
    Signed-off-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,25 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+  mode: process.env.WEBPACK_SERVE ? 'development' : 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'public/index.html'
+      template: 'public/index.html',
     })
-  ]
+  ],
+  serve: {
+    port: 8000
+  }
 }
